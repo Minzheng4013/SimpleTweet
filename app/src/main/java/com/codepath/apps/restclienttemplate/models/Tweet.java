0a4fb3d740723fc5a554038ffff52750cc0fde22
Minzheng4013 <mimizheng4013@gmail.com>
2022-03-04 23:30:24 -0500

    Initial commit

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -5,10 +5,11 @@ import android.webkit.JsPromptResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Tweet {
     public String body;
     public String createAt;
@@ -16,6 +17,7 @@ public class Tweet {
     public User user;
 
     //empty constructor needed by the parceler library
+
     public  Tweet() {}
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
